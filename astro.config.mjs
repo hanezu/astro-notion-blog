b4b4bd2e8a42fcaf8435f9533e956b914f16e651
hanezu <tim.zhc@gmail.com>
2023-09-14 00:02:41 +0900

    add dotenv for convenience

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,9 @@ import CoverImageDownloader from './src/integrations/cover-image-downloader';
 import CustomIconDownloader from './src/integrations/custom-icon-downloader';
 import FeaturedImageDownloader from './src/integrations/featured-image-downloader';
 import PublicNotionCopier from './src/integrations/public-notion-copier';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const getSite = function () {
   if (CUSTOM_DOMAIN) {
